4ed12b62ea6e6faef3e3df71b1d9f61bbe90c9c0
Maxence Albaut <maxencealbaut@gmail.com>
2021-11-19 18:56:10 +0100

    changed url split to urlsearchparams

--- front/js/confirm.js
+++ front/js/confirm.js
@@ -1,6 +1,11 @@
 
+// Recupere l'id de la commande depuis l'url
+let url = window.location.search
+let urlParams = new URLSearchParams(url)
+let orderId = urlParams.get('orderId')
+
 let idTag = document.getElementById('orderId')
 
 if (idTag){
-    idTag.innerHTML = document.URL.split('?')[1]
+    idTag.innerHTML = orderId
 }
